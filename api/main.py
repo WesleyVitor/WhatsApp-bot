08fc2d96e5a5fa549e66c2f38552c506b00d72bc
WesleyVitor <wesleydemorais@outlook.com.br>
2025-11-27 21:16:10 -0300

    feat: using twilio to send whatsapp message

--- api/main.py
+++ api/main.py
@@ -1,11 +1,14 @@
 
+import os
 
 from workflow import create_graph
 from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
-from client import InfobipClient
+from fastapi.requests import Request
+from twilio.rest import Client
+
 load_dotenv(override=True)
 
 app = FastAPI(title="WhatsApp Bot API", version="1.0.0")
@@ -18,38 +21,51 @@ app.add_middleware(
     allow_headers=["*"],
 )
 class RequestBody(BaseModel):
-    question: str
+    user_name: str
     user_number: str
 
-class ResponseBody(BaseModel):
-    success_info: str
-    failed_info: str
 
-@app.post("/answer_something")
-async def answer_something(body: RequestBody):
+@app.post("/send_twilio_template")
+async def send_twilio_template(body: RequestBody):
     """
-    Endpoint to call chatbot
+    Sent initial template
     """
+
+    
+    account_sid = os.environ.get("ACCOUNT_SID")
+    auth_token = os.environ.get("AUTH_TOKEN")
+    content_sid = os.environ.get("CONTENT_SID")
+    from_number = os.environ.get("FROM_NUMBER")
+    to_number = body.user_number
+    user_name = body.user_name
+    client = Client(account_sid, auth_token)
+
+    client.messages.create(
+        from_=f'whatsapp:{from_number}',
+        content_sid=content_sid,
+        content_variables=f'{"1":{user_name}}',
+        to=f'whatsapp:+{to_number}'
+    )
+    
+    return {}
+
+@app.post("/reply_whatsapp")
+async def reply_whatsapp(request: Request):
+    
+    payload = await request.form()
+    message = payload.get("Body")
+    sender = payload.get("From")
     graph = create_graph()
     
     initial_state = {
-        "messages": [{"role":"user", "content":body.question}],
-        "user_number": body.user_number
+        "messages": [{"role":"user", "content":message}],
+        "user_number": sender
     }
     
     await graph.ainvoke(initial_state)
 
-    return ResponseBody(success_info="Message sent successful!")
+    return {}
 
-@app.post("/send_template")
-async def send_template(body: RequestBody):
-    """
-    Sent initial template
-    """
-    client = InfobipClient()
-    client.send_initial_template(body.user_number)
-    
-    return ResponseBody(success_info="Message sent successful!")
 
 @app.post("/hello")
 def hello():
--- api/nodes.py
+++ api/nodes.py
@@ -1,14 +1,11 @@
 
 import os
-import requests
-import json
 
-from client import InfobipClient
 from langgraph.graph.message import add_messages
 from typing import TypedDict, Annotated
 from agents import Agent, Runner, trace
 from agents.mcp import MCPServerStdio
-
+from twilio.rest import Client
 
 class GraphState(TypedDict):
     messages: Annotated[list, add_messages]
@@ -49,15 +46,23 @@ async def process_message(state: GraphState) -> GraphState:
 
     return new_state
 
+
 def send_whatsapp_message(state: GraphState) -> GraphState:
     """
     SendWhatsappMessage node that send the final message from LLM to whatsapp
 
     """
-    #receive_number = os.environ.get("MY_NUMBER")
     receive_number = state.get("user_number")
     last_message = state.get("messages")[-1].content
 
-    client = InfobipClient()
-    client.send_text_message(receive_number, last_message)
+    account_sid = os.environ.get("ACCOUNT_SID")
+    auth_token = os.environ.get("AUTH_TOKEN")
+    from_number = os.environ.get("FROM_NUMBER")
+    client = Client(account_sid, auth_token)
+
+    client.messages.create(
+        from_=f'whatsapp:{from_number}',
+        body=last_message,
+        to=receive_number
+    )
     return state
